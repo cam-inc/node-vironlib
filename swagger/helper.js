d6d2b33a0bb14fc9c3bf4ac153f3a1e2ef8d5820
goOmura <omura@camobile.com>
2017-08-31 16:36:25 +0900

    Update: swagger helper autoGenerate

--- swagger/helper.js
+++ swagger/helper.js
@@ -16,29 +16,54 @@ const getPort = (swaggerExpress, defaultValue) => {
 /**
  * AdminRole create/update 用のenum配列を自動生成する
  * @param swaggerExpress
+ * @returns {Promise.<TResult>}
  */
 const genAdminRolePaths = swaggerExpress => {
-  const paths = swaggerExpress.runner.swagger.paths;
-  const enums = new Set();
-  for (let path in paths) {
-    for (let method in paths[path]) {
-      const resource = path.split('/')[1];
-      // ホワイトリストに入っているリソースは無視
-      if (resource && !contains(adminRoleHelper.whiteList, resource)) {
-        enums.add(`${method.toUpperCase()}:/${resource}`);
+  return new Promise(resolve => {
+    const paths = swaggerExpress.runner.swagger.paths;
+    const enums = new Set();
+    for (let path in paths) {
+      for (let method in paths[path]) {
+        const resource = path.split('/')[1];
+        // ホワイトリストに入っているリソースは無視
+        if (resource && !contains(adminRoleHelper.whiteList, resource)) {
+          enums.add(`${method.toUpperCase()}:/${resource}`);
+        }
       }
     }
-  }
-  const def = swaggerExpress.runner.swagger.definitions.adminrolepath;
-  def.properties.path.enum = Array.from(enums);
+    const def = swaggerExpress.runner.swagger.definitions.adminrolepath;
+    def.properties.path.enum = Array.from(enums);
+    resolve();
+  });
+};
+
+/**
+ * AdminUser create/update 用のenum配列を自動生成する
+ * @param swaggerExpress
+ * @returns {Promise.<TResult>}
+ */
+const genAdminUserRoleId = (swaggerExpress, AdminRoles) => {
+  return AdminRoles.findAll()
+    .then(list => {
+      const enums = new Set();
+      list.forEach(role => {
+        enums.add(role.dataValues.role_id);
+      });
+      const def = swaggerExpress.runner.swagger.definitions.UpdateAdminUserPayload;
+      def.properties.role_id.enum = Array.from(enums);
+    })
+  ;
 };
 
 /**
  * Auto Generate swagger.json
  * @param swaggerExpress
  */
-const autoGenerate = swaggerExpress => {
-  genAdminRolePaths(swaggerExpress);
+const autoGenerate = (swaggerExpress, models) => {
+  return Promise.all([
+    genAdminRolePaths(swaggerExpress),
+    genAdminUserRoleId(swaggerExpress, models.AdminRoles)
+  ]);
 };
 
 module.exports = {
