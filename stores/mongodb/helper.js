92b8696206141f6f412dc1bf9fb543217b7fd25f
fkei <kei.topaz@gmail.com>
2019-03-06 20:21:09 +0900

    WIP: 🚧 Mongo support

--- stores/mongodb/helper.js
+++ stores/mongodb/helper.js
@@ -1,106 +1,124 @@
 const isEmpty = require('mout/lang/isEmpty');
 const isFinite = require('mout/lang/isFinite');
-
 const constants = require('../../constants');
+const mongoose = require('mongoose');
 
 /**
  * find
- * @param {Mongoose.Model} model
- * @param {Object} query - {name: "a"}
- * @param {Object} options
- * @param {Array} options.attributes - ['id', 'name', 'age']
- * @param {Number} options.limit - 100
- * @param {Number} options.offset - 10
- * @param {Array} options.order - [['id', 'ASC'], ['name', 'DESC']]
+ * @param {Mongoose.Model} Model
+ * @param {Object} conditions - {name: "a"}
+ * @param {Object} projection - ['id', 'name', 'age']
+ * @param {Object} options - {limit: 1, offset: 0, sort: {createdAt: 'desc'}}
  */
-const find = (model, query, options={}) => {
-  debugger;
-  // const opts = Object.assign({}, options);
-  // if (!isEmpty(query)) {
-  //   opts.where = query;
-  // }
-  // if (!isFinite(options.limit)) {
-  //   opts.limit = constants.DEFAULT_PAGER_LIMIT;
-  // }
-  // if (!isFinite(options.offset)) {
-  //   opts.offset = 0;
-  // }
-  // return model.findAll(opts);
+const find = (Model, conditions = {}, projection = {}, options = {}) => {
+  const opts = Object.assign({}, options);
+  if (!isFinite(options.limit)) {
+    opts.limit = constants.DEFAULT_PAGER_LIMIT;
+  }
+  if (!isFinite(options.skip)) {
+    opts.skip = 0;
+  }
+  if (isEmpty(options.sort)) {
+    opts.sort = {
+      createdAt: 'desc'
+    };
+  }
+  return Model.find(conditions, projection, opts).exec();
 };
 
 /**
  * findOne
- * @param {Mongoose.Model} model
- * @param {Object} query - {id: 1}
- * @param {Object} options
- * @param {Array} options.attributes - ['id', 'name', 'age']
+ * @param {Mongoose.Model} Model
+ * @param {Object} conditions 検索条件
+ * @param {Object} projection 結果データフィルタ
+ * @param {Object} options Mongooseオプション
  */
-const findOne = (model, query, options={}) => {
-  debugger;
-  // const opts = Object.assign({}, options);
-  // if (!isEmpty(query)) {
-  //   opts.where = query;
-  // }
-  // return model.findOne(opts);
+const findOne = (Model, conditions, projection = {}, options = {}) => {
+  return Model.findOne(conditions, projection, options);
 };
 
 /**
  * count
- * @param {Mongoose.Model} model
- * @param {Object} query - {name: 'AAA'}
- * @param {Object} options
+ * @param {Mongoose.Model} Model
+ * @param {Object} conditions 検索条件
  */
-const count = (model, query, options={}) => {
-  debugger;
-  // const opts = Object.assign({}, options);
-  // if (!isEmpty(query)) {
-  //   opts.where = query;
-  // }
-  // delete opts.attributes;
-  // return model.count(opts);
+const count = (Model, conditions = {}) => {
+  return Model.countDocuments(conditions);
 };
 
 /**
  * create
- * @param {Mongoose.Model} model
- * @param {Object} data
+ * @param {Mongoose.Model} Model
+ * @param {Object} doc 保存するデータ
  */
-const create = (model, data) => {
-  debugger;
-  return model.create(data);
+const create = (Model, doc) => {
+  return new Model(doc).save();
+};
+
+/**
+ * remove
+ * @param {Mongoose.Model} Model
+ * @param {Object} conditions 検索条件
+ */
+const remove = (Model, conditions) => {
+  return Model.remove(conditions);
 };
 
 /**
  * remove
  * @param {Mongoose.Model} model
- * @param {Object} query - {name: 'AAA'}
- * @param {Object} options
- * @param {boolean} options.force
+ * @param {Object} conditions 検索条件
+ * @param {*} options Mongooseオプション
  */
-const remove = (model, query, options={}) => {
-  debugger;
-  // const opts = Object.assign({}, options);
-  // if (!isEmpty(query)) {
-  //   opts.where = query;
-  // }
-  // return model.destroy(opts);
+const removeOne = (Model, conditions, options = {}) => {
+  return Model.findOneAndRemove(conditions, options);
 };
-
 /**
  * update
  * @param {Mongoose.Model} model
- * @param {Object} query - {name: 'AAA'}
- * @param {Object} data
- * @param {Object} options
+ * @param {*} query 検索条件
+ * @param {*} doc 保存するデータ
+ * @param {*} options Mongooseオプション
  */
-const update = (model, query, data, options={}) => {
-  debugger;
-  // const opts = Object.assign({}, options);
-  // if (!isEmpty(query)) {
-  //   opts.where = query;
-  // }
-  // return model.update(data, opts);
+const updateOne = (Model, query, doc, options = {true: false}) => {
+  return Model.findOneAndUpdate(query, doc, options);
 };
+
+/**
+ *
+ * @param {Mongoose.Model} model
+ * @param {Object} conditions 検索条件
+ * @param {*} projection 結果データフィルタ
+ * @param {*} options Mongooseオプション
+ */
+const findWithCount = async (
+  Model,
+  conditions = {},
+  projection = {},
+  options = {}
+) => {
+  const [list, totalCount] = await Promise.all([
+    find(Model, conditions, projection, options),
+    count(Model, conditions)
+  ]);
+
+  return {
+    list: list.map(obj => obj.toJSON()),
+    count: totalCount,
+  };
+};
+
+/**
+ * MongoDB#ObjectIDかどうか
+ * @param {*} s チェック対応データ
+ */
+const isObjectId = s => {
+  if (s instanceof mongoose.Types.ObjectId) {
+    return true;
+  }
+  return false;
+};
+
 
 module.exports = {
   find,
@@ -108,5 +126,8 @@ module.exports = {
   count,
   create,
   remove,
-  update,
+  removeOne,
+  updateOne,
+  findWithCount,
+  isObjectId
 };
